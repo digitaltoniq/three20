921df97fca590fc1db2b7021e80806495e7a440b
Daniel Snider <dsnider@digitaltoniq.com>
2010-10-03 14:30:16 -0400

    Modification to allow 3.2 compilation

--- src/Three20UINavigator/Sources/TTBaseNavigator.m
+++ src/Three20UINavigator/Sources/TTBaseNavigator.m
@@ -49,7 +49,7 @@ static NSString* kNavigatorHistoryLastSessionAsymetricKey  = @"TTNavigatorHistor
 
 UIKIT_EXTERN NSString *const UIApplicationDidEnterBackgroundNotification __attribute__((weak_import));
 UIKIT_EXTERN NSString *const UIApplicationWillEnterForegroundNotification __attribute__((weak_import));
- 
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -77,12 +77,14 @@ UIKIT_EXTERN NSString *const UIApplicationWillEnterForegroundNotification __attr
                selector:@selector(applicationWillLeaveForeground:)
                    name:UIApplicationWillTerminateNotification
                  object:nil];
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 40000
     if (nil != &UIApplicationDidEnterBackgroundNotification) {
       [center addObserver:self
                  selector:@selector(applicationWillLeaveForeground:)
                      name:UIApplicationDidEnterBackgroundNotification
                    object:nil];
     }
+#endif
   }
   return self;
 }
